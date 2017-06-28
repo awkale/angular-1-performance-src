b495647e33d6bb0be27c035824c875e6d9e597e4
Alex Kale <hi@awkale.me>
2017-06-28 16:27:06 -0400

    lesson 8 notes

--- 08-ng-repeat-batching/js/components/todos.component.js
+++ 08-ng-repeat-batching/js/components/todos.component.js
@@ -23,6 +23,7 @@ var todos = {
       this.todos = [];
     };
     this.renderBatched = function () {
+      // custom function; 1st arg pass in string becomes a key to able to look up inside  chunkAndBatchRender method if there are multiple collections in a controller to make it resuable; 2nd arg todo service to pass in collection as array of data; 3rd arg how to divide array
       this.chunkAndBatchRender('todos', TodoService.getTodos(), 25);
     };
     this.renderAll = function () {
@@ -42,8 +43,10 @@ var todos = {
     this.chunkAndBatchRender = function (hash, collection, size) {
 
       var _this = this;
+      // gives promise object
       var promise = $q.resolve();
 
+      // splits in to chunks
       function chunkCollection(collection, size) {
         var chunks = [];
         for (var i = 0; i < collection.length; i += size) {
@@ -52,16 +55,22 @@ var todos = {
         return chunks;
       }
 
+      // calculate how to schedule and paint to DOM; returns a promise
       function scheduleRender(chunk) {
         Array.prototype.push.apply(_this[hash], chunk);
+        //$timeout returns promise
         return $timeout(function () {}, 0);
       }
 
+      // value is 48 arrays of 25 values in each
       var chunked = chunkCollection(collection, size);
 
       var nextBatch;
+      //keep track on which batch being used; use promise to schedule the next batch once the first one complete
       chunked.forEach(function(chunk, index) {
+        // pass the chunk being iterated over and apply to array
         nextBatch = scheduleRender.bind(null, chunk);
+        // time will be returned to the .then
         promise = promise.then(nextBatch);
       });
 
